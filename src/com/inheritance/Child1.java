de04325725c1d6f45e5a4b3097a5c857a1e65eb8
aryamann <arya00014@gmail.com>
2021-01-17 22:47:19 +0530

    INHERITANCE

---
+++ src/com/inheritance/Child1.java
@@ -0,0 +1,18 @@
+package com.inheritance;
+
+public class Child1 extends Parent {
+	public String getname() {
+		return "Aryamann";
+
+	}
+
+	public String getcar() {
+		return "Lamborghini";
+
+	}
+
+	public String getBloodgroup() {
+		return "B+ve";
+	}
+
+}
